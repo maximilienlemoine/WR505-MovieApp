f787e21bf00924f0b46618a34dfc0bb83b0ef91a
Maximilien <maximilien.lemoine.pro@gmail.com>
2024-02-17 20:01:22 +0100

    fix : install arco-design

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import '@arco-design/web-vue/dist/arco.css'
 
 import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
+import ArcoVue from '@arco-design/web-vue';
+
 
 const app = createApp(App)
 
 app.use(router)
+app.use(ArcoVue)
 app.mount('#app')
