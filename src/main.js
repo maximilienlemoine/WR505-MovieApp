18a60bec20f22d345fcd7cb5a364b0912d79c276
Maximilien <maximilien.lemoine.pro@gmail.com>
2024-02-17 19:36:09 +0100

    feat : add boostrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 import {createApp} from 'vue'
 import App from './App.vue'
